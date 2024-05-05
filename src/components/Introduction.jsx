6762a5f0f3cfcbd43a1f3bd51988f115c4532127
trung <htrung11120@gmail.com>
2024-05-04 22:57:08 -0400

    updated introdfuction line

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -4,12 +4,14 @@ export default function Introduction() {
     <div className={Styled.container}>
       <div className={Styled.textContainer}>
         <p className={Styled.hello}>Hello.</p>
-        <p className={Styled.name}>
-          My name is <span>Hoai Tran</span>
-        </p>
+
         <p className={Styled.selfIntro}>
-          I am a FrontEnd developer with a passion to create new things. Welcome
-          to my portfolio, let's create something remarkable together.
+          <span className={Styled.name}>
+            My name is <span>Hoai Tran</span>
+          </span>
+          , a passionate FrontEnd developer dedicated to crafting exceptional
+          user experiences. Seeking new opportunities to contribute my skills
+          and creativity. Let's create something remarkable together.
         </p>
       </div>
     </div>
