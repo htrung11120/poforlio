892bd95e8546c2d513db47c8844460e202c7764b
trung <htrung11120@gmail.com>
2024-05-06 15:37:59 -0400

    adding mobile responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
 import Styled from "./header.module.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
 export default function Header() {
+  const [boolean, setBoolean] = useState(false);
+  const display = () => {
+    setBoolean(!boolean);
+  };
   return (
     <div className={Styled.container}>
       <span className={Styled.name}>HOAI TRAN</span>
+      {!boolean && (
+        <div className={Styled.mobileNav}>
+          <GiHamburgerMenu className={Styled.icon} onClick={display} />
+        </div>
+      )}
+
       <nav>
         <ul className={Styled.list}>
           <a href="#about">
@@ -20,6 +32,25 @@ export default function Header() {
           </a>
         </ul>
       </nav>
+      {boolean && (
+        <div className={Styled.mobileNavItem}>
+          <ul className={Styled.MobileList}>
+            <a href="#about" onClick={display}>
+              <li>About</li>
+            </a>
+            <a href="#skills" onClick={display}>
+              <li>Skills</li>
+            </a>
+            <a href="#projects" onClick={display}>
+              <li>Projects</li>
+            </a>
+
+            <a href="#contact" onClick={display}>
+              <li>Contact</li>
+            </a>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
