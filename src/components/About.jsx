3d149aadff4d2d5782bbba0fafd85bdc4fab47e7
trung <htrung11120@gmail.com>
2024-05-07 19:45:22 -0400

    add about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,10 +11,12 @@ export default function About() {
     <div className={Style.container} id="about">
       <p className={Style.heading}>About Me</p>
       <p className={Style.paragrpah}>
-        I'm Hoai Tran, a FrontEnd developer blending precision engineering with
-        creative coding. Proficient in HTML, CSS, JavaScript, and React, I
+        I'm Hoai Tran, a FrontEnd developer who combines precision engineering
+        with creative coding. Proficient in HTML, CSS, JavaScript, and React, I
         specialize in crafting immersive digital experiences. Let's collaborate
-        and create something extraordinary!
+        and create something extraordinary! Additionally, I am currently
+        learning Node.js and MongoDB to become a JavaScript full-stack
+        developer.
       </p>
     </div>
   );
